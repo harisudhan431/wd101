3b494db4edf7f5cf6b39224e37e7b5ce1acdcfc8
harisudhan431 <118600901+harisudhan431@users.noreply.github.com>
2023-07-31 12:26:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 
-    document.getElementById('registrationForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const dob = document.getElementById('dob').value;
-      const acceptedTerms = document.getElementById('acceptTerms').checked;
+document.addEventListener("DOMContentLoaded", function () {
+  // Load previous entries from local storage
+  const savedEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+  displayEntries(savedEntries);
+});
 
-      const tableBody = document.querySelector('#userTable tbody');
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-        <td>${name}</td>
-        <td>${email}</td>
-        <td>${password}</td>
-        <td>${dob}</td>
-        <td>${acceptedTerms ? 'Yes' : 'No'}</td>
-      `;
-      tableBody.appendChild(newRow);
-      document.getElementById('registrationForm').reset();
-    });
+function addEntry() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptedTerms = document.getElementById("acceptedTerms").checked;
 
+  if (!isValidEmail(email)) {
+    alert("Invalid email address.");
+    return;
+  }
+
+  const dobDate = new Date(dob);
+  const currentDate = new Date();
+  const age = currentDate.getFullYear() - dobDate.getFullYear();
+  if (age < 18 || age > 55) {
+    alert("Age must be between 18 and 55 years.");
+    return;
+  }
+
+  const entry = {
+    name,
+    email,
+    password,
+    dob,
+    acceptedTerms,
+  };
+
+  // Save the new entry to local storage
+  const savedEntries = JSON.parse(localStorage.getItem("userEntries")) || [];
+  savedEntries.push(entry);
+  localStorage.setItem("userEntries", JSON.stringify(savedEntries));
+
+  // Display the new entry in the table
+  displayEntries(savedEntries);
+}
+
+function displayEntries(entries) {
+  const tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = "";
+  entries.forEach(function (entry) {
+    const newRow = tableBody.insertRow();
+    newRow.innerHTML = `
+      <td>${entry.name}</td>
+      <td>${entry.email}</td>
+      <td>${entry.password}</td>
+      <td>${entry.dob}</td>
+      <td>${entry.acceptedTerms ? "Yes" : "No"}</td>
+    `;
+  });
+}
+
+function isValidEmail(email) {
+  // Implement email validation logic here (regular expression, etc.)
+  return true; // Return true for simplicity in this example
+}
