b7eec3ccf686a53e7721f8d587709f710c31dbe5
harisudhan431 <118600901+harisudhan431@users.noreply.github.com>
2023-07-31 11:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 
     document.getElementById('registrationForm').addEventListener('submit', function(event) {
       event.preventDefault();
+      addUser();
+    });
+
+    function addUser() {
       const name = document.getElementById('name').value;
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
       const dob = document.getElementById('dob').value;
-      const acceptedTerms = document.getElementById('acceptTerms').checked;
+      const acceptedTerms = document.getElementById('acceptedTerms').checked;
 
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        acceptedTerms: acceptedTerms,
+      };
+
+      const users = JSON.parse(localStorage.getItem('users') || '[]');
+      users.push(user);
+      localStorage.setItem('users', JSON.stringify(users));
+
+      displayUsers();
+    }
+
+    function displayUsers() {
+      const users = JSON.parse(localStorage.getItem('users') || '[]');
       const tableBody = document.querySelector('#userTable tbody');
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-        <td>${name}</td>
-        <td>${email}</td>
-        <td>${password}</td>
-        <td>${dob}</td>
-        <td>${acceptedTerms ? 'Yes' : 'No'}</td>
-      `;
-      tableBody.appendChild(newRow);
-      document.getElementById('registrationForm').reset();
-    });
+      tableBody.innerHTML = '';
+
+      users.forEach(function(user) {
+        const newRow = document.createElement('tr');
+        newRow.innerHTML = `
+          <td>${user.name}</td>
+          <td>${user.email}</td>
+          <td>${user.password}</td>
+          <td>${user.dob}</td>
+          <td>${user.acceptedTerms ? 'Yes' : 'No'}</td>
+        `;
+        tableBody.appendChild(newRow);
+      });
+    }
+
+    displayUsers();
